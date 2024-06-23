95a83d57ecbd80db999bc7e852a8a3ebe798c6de
Omkar Patil <git21cs072-t@students.git.edu>
2024-06-23 12:21:34 +0530

    updated TW3

--- USP Termworks/tw33.cpp
+++ USP Termworks/tw33.cpp
@@ -1,18 +1,11 @@
 //termwork 3 emulate unix
 
 
- #include<iostream>
-#include<sys/types.h>
-#include<unistd.h>
-#include<string.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main( int argc , char* argv[] )
  {
-   if( ((argc<3) || (argc>4)) || (argc==4 && strcmp(argv[1],"-s")) )
-    { 
-      cerr<<"Usage : "<< argv[0]<<" [-s] <orig_file> <new_file> \n";
-      return 1;
-    }
+   
    if( argc == 4 )   // programName -s <source_file> <destination_file>
     {
       if( symlink( argv[2] ,argv[3] ) == -1 )
